c904234721962a97388c765476303f68f72dbfb7
yperez0914 <71104698+yperez0914@users.noreply.github.com>
2021-02-09 22:28:36 -0500

    begin filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,19 +5,6 @@ var tbody = d3.select("tbody");
 // Console.log the weather data from data.js
 console.log(tabledata);
 
-// data.forEach(function(sightings) {
-//     console.log(sightings);
-//     var row = tbody.append("tr");
-    
-//     Object.entries(sightings).forEach(function([key, value]) {
-//         console.log(key, value);
-//         // Append a cell to the row for each value
-//         // in the weather report object
-//     var cell = row.append("td");
-//  cell.text(value);
-// });
-// });
-
 data.forEach((sightings) => {
     var row = tbody.append("tr");
     Object.entries(sightings).forEach(([key, value]) => {
@@ -25,3 +12,31 @@ data.forEach((sightings) => {
       cell.text(value);
     });
   });
+  // Select the button
+  var button = d3.select("#button");
+
+  // Select the form
+  var form = d3.select("#form");
+  // Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(sightings);
+
+  var filteredData = sightings.filter(date => date.datetime === inputValue);
+
+  console.log(filteredData);
+};
